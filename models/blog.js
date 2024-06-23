6f2462b0cc1acd7377a8d7cdc3e58fcf412fea04
adityaagrawal53 <aditya@DellA2>
2024-06-23 16:51:24 +0530

    refactored code (commited using npm script)

---
+++ models/blog.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+
+
+module.exports = mongoose.model('Blog', blogSchema)
